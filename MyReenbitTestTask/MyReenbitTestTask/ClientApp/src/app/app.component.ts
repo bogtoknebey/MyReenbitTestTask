91843c09b8c1e23371c018d87e33910cedb1f485
Valeriy <valerii.yaremenko@nure.ua>
2023-09-14 21:26:35 +0200

    initial take
    .

---
+++ MyReenbitTestTask/MyReenbitTestTask/ClientApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  emailError: boolean = false;
+  fileError: boolean = false;
+
+}
